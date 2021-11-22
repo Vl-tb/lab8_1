c3269a18a3a318594f6bff88e3388e0cc8f75374
Vladyslav Protsenko <vladyslav.protsenko@ucu.edu.ua>
2021-11-23 01:32:08 +0200

    Fixed test

--- src/test/java/mail_types/ClientTest.java
+++ src/test/java/mail_types/ClientTest.java
@@ -12,15 +12,16 @@ public class ClientTest {
 
     @Before
     public void setUp() throws Exception {
-        client1 = new Client("Vlad", Gender.MALE, 19);
-        client2 = new Client("Valera", Gender.MALE, 29);
-        client3 = new Client("Stepan", Gender.MALE, 43);
+
     }
 
     @Test
     public void getId() {
+        client1 = new Client("Vlad", Gender.MALE, 19);
         assertEquals(client1.getId(), 0);
+        client2 = new Client("Valera", Gender.MALE, 29);
         assertEquals(client2.getId(), 1);
+        client3 = new Client("Stepan", Gender.MALE, 43);
         assertEquals(client3.getId(), 2);
     }
 }
