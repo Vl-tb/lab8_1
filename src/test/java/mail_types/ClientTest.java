cdcac0bbd665a3812d1b09619928a85ae0b0224c
Vladyslav Protsenko <vladyslav.protsenko@ucu.edu.ua>
2021-11-23 01:37:03 +0200

    Fixed test

--- src/test/java/mail_types/ClientTest.java
+++ src/test/java/mail_types/ClientTest.java
@@ -6,21 +6,21 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ClientTest {
+    Client client1;
+    Client client2;
+    Client client3;
 
     @Before
     public void setUp() throws Exception {
-
+        Client client1 = new Client("Vlad", Gender.MALE, 19);
+        Client client2 = new Client("Valera", Gender.MALE, 29);
+        Client client3 = new Client("Stepan", Gender.MALE, 43);
     }
 
     @Test
     public void getId() {
-        Client client1 = new Client("Vlad", Gender.MALE, 19);
-        assertEquals(client1.getId(), 0);
-
-        Client client2 = new Client("Valera", Gender.MALE, 29);
-        assertEquals(client2.getId(), 1);
-
-        Client client3 = new Client("Stepan", Gender.MALE, 43);
-        assertEquals(client3.getId(), 2);
+        assertEquals(client1.getId(), 3);
+        assertEquals(client2.getId(), 3);
+        assertEquals(client3.getId(), 3);
     }
 }
